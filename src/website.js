8003d97b6ba6638e2c9ca3fc32c645d45df452e4
Ryan Hetherington <hetheringtonr2@comcast.net>
2021-09-26 15:10:33 -0700

    fourth commit

--- src/website.js
+++ src/website.js
@@ -17,18 +17,89 @@ function createHeader() {
 
 function createNav() {
     const nav = document.createElement("nav");
+    nav.setAttribute("id", "navbar");
     const list = document.createElement("ul");
-    const listItem = document.createElement("li");
+    list.classList.add("navbar-list");
     nav.appendChild(list);
-    listItem.appendChild(document.createTextNode("Five"));
-    list.appendChild(listItem);
+    
+    let listItemOne = document.createElement("li");
+    listItemOne.classList.add("navbar-link");
+    list.appendChild(listItemOne);
+
+    let listItemTwo = document.createElement("li");
+    listItemTwo.classList.add("navbar-link");
+    list.appendChild(listItemTwo);
+
+    let listItemThree = document.createElement("li");
+    listItemThree.classList.add("navbar-link");
+    list.appendChild(listItemThree);
+
+    let homeBtn = document.createElement("button");
+    homeBtn.classList.add("nav-button");
+    homeBtn.textContent = "Home";
+    listItemOne.appendChild(homeBtn);
+    homeBtn.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActive(homeBtn);
+    });
+    
+
+    let menuBtn = document.createElement("button");
+    menuBtn.classList.add("nav-button");
+    menuBtn.textContent = "Menu";
+    listItemTwo.appendChild(menuBtn);
+    menuBtn.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActive(menuBtn);
+    });
+
+    let contactBtn = document.createElement("button");
+    contactBtn.classList.add("nav-button");
+    contactBtn.textContent = "Contact";
+    listItemThree.appendChild(contactBtn);
+    contactBtn.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActive(contactBtn);
+    });
 
     return nav;
 }
+
+function setActive(button) {
+    const buttonList = document.querySelectorAll(".nav-button");
+
+    buttonList.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove("active");
+        }
+    });
+
+    button.classList.add("active");
+}
+
+function createMain() {
+    const main = document.createElement("main");
+    main.setAttribute("id", "main");
+    return main;
+}
+
+function createFooter() {
+    const footer = document.createElement("footer");
+    footer.setAttribute("id", "footer");
+
+    const copyright = document.createElement("span");
+    copyright.classList.add("copyright");
+    copyright.textContent = "Copyright © 2021 | Ryan-C892";
+    footer.appendChild(copyright);
+    return footer;
+}
+
 function loadWebsite() {
     const content = document.getElementById("content");
     content.textContent ="";
     content.appendChild(createHeader());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
 }
 
 export default loadWebsite;
